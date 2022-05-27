dfa06e81848cda2a35b18e13662621a18bfff1ea
moumou <hielf1984@gmail.com>
2022-05-26 17:11:58 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'hello_world', to: 'hello_world#index'
-  resource :wechat, only: [:show, :create]
   resources :users
   namespace :api, defaults: {format: :json} do
     root 'root#home'
@@ -9,10 +7,6 @@ Rails.application.routes.draw do
     post 'accounts/miniprogram_sign_in', to: 'accounts#miniprogram_sign_in'
     post 'accounts/simple_sign_in', to: 'accounts#simple_sign_in'
     post 'accounts/sign_out', to: 'accounts#sign_out'
-    get 'wechat_access_token', to: 'root#wechat_access_token'
-    get 'wechat_userinfo', to: 'root#wechat_userinfo'
-    get 'miniprogram_openid', to: 'root#miniprogram_openid'
-    get 'wechat_get_token', to: 'root#wechat_get_token'
     resources :users, except: [:edit, :new] do
       collection do
         post :send_verify
@@ -29,13 +23,6 @@ Rails.application.routes.draw do
       collection do
         post :pre_pay
         post :notify
-      end
-    end
-    resources :stock_lists do
-      collection do
-        get :user_history
-        get :market_quotations
-        get :stock_analysis_results
       end
     end
     # match '*path', to: 'root#route_not_found', via: :all
