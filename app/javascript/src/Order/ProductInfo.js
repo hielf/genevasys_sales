96e84567cf3634a894101002f02fc405336a0e25
moumou <hielf1984@gmail.com>
2022-06-03 14:10:05 -0700

    update page

--- app/javascript/src/Order/ProductInfo.js
+++ app/javascript/src/Order/ProductInfo.js
@@ -7,49 +7,106 @@ const onChange = (checkedValues) => {
   console.log('checked = ', checkedValues);
 };
 
-const optionsInternet = [
-  {
-    label: 'Internet 75: $55.95/month',
-    value: 'A1',
-  },
-  {
-    label: 'Internet 300: $85.95/month',
-    value: 'A2',
-  },
-  {
-    label: 'Internet 750: $105.95/month',
-    value: 'A3',
-  },
-  {
-    label: 'Internet 750: $105.95/month',
-    value: 'A4',
-  },
-];
+const list = {
+  Bundle: [
+    {
+      label: 'Internet + TV Box + IP Phone 75: $55.95/month',
+      value: 'A1',
+    },
+    {
+      label: 'Internet + TV Box + IP Phone 300: $85.95/month',
+      value: 'A2',
+    },
+    {
+      label: 'Internet + TV Box + IP Phone 750: $105.95/month',
+      value: 'A3',
+    },
+    {
+      label: 'Internet + TV Box + IP Phone 750: $105.95/month',
+      value: 'A4',
+    },
+  ],
+  Internet: [
+    {
+      label: 'Internet 75: $55.95/month',
+      value: 'A5',
+    },
+    {
+      label: 'Internet 300: $85.95/month',
+      value: 'A6',
+    },
+    {
+      label: 'Internet 750: $105.95/month',
+      value: 'A7',
+    },
+    {
+      label: 'Internet 750: $105.95/month',
+      value: 'A8',
+    },
+  ],
+  'TV Box': [
+    {
+      label: 'TV Box 75: $55.95/month',
+      value: 'A9',
+    },
+    {
+      label: 'TV Box 300: $85.95/month',
+      value: 'A21',
+    },
+    {
+      label: 'TV Box 750: $105.95/month',
+      value: 'A31',
+    },
+    {
+      label: 'TV Box 750: $105.95/month',
+      value: 'A41',
+    },
+  ],
+  'IP Phone': [
+    {
+      label: 'IP Phone 75: $55.95/month',
+      value: 'A111',
+    },
+    {
+      label: 'IP Phone 300: $85.95/month',
+      value: 'A211',
+    },
+    {
+      label: 'IP Phone 750: $105.95/month',
+      value: 'A311',
+    },
+    {
+      label: 'IP Phone 750: $105.95/month',
+      value: 'A411',
+    },
+  ],
+};
 
 const ProductInfo = () => (
   <>
   <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-    <Divider orientation="left">Bundle</Divider>
     <Checkbox.Group onChange={onChange}>
-      <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-        {optionsInternet.map(d => (
-          <Row key={d.value}>
-            <Col span={24}>
-              <Checkbox value={d.value}>{d.label}</Checkbox>
-            </Col>
-          </Row>
-        ))}
-      </Space>
+    {
+      Object.keys(list).map(p => {
+        return (
+          <Space direction='vertical' size='middle' style={{ display: 'flex' }}>
+            <Divider orientation='left' key={ p }>{p}</Divider>
+            {
+              list[p].map(d => {
+                return (
+                  <Row key={d.value}>
+                    <Col span={24}>
+                      <Checkbox value={d.value}>{d.label}</Checkbox>
+                    </Col>
+                  </Row>
+                )
+              })
+            }
+          </Space>
+        )
+      })
+    }
     </Checkbox.Group>
-
-    <Divider orientation="left">Internet</Divider>
-    <Checkbox.Group options={optionsInternet} onChange={onChange} />
-
-    <Divider orientation="left">TV Box</Divider>
-    <Checkbox.Group options={optionsInternet} onChange={onChange} />
-
-    <Divider orientation="left">IP Phone</Divider>
-    <Checkbox.Group options={optionsInternet} onChange={onChange} />
   </Space>
   </>
 );
