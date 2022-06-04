bd5724ea40548ef09b781b26ad23cec35ed72284
moumou <hielf1984@gmail.com>
2022-06-03 17:22:49 -0700

    Update ProgressBar.js

--- app/javascript/src/Components/ProgressBar.js
+++ app/javascript/src/Components/ProgressBar.js
@@ -3,7 +3,7 @@ import { Progress } from 'antd';
 
 const ProgressBar = (progress, FormTitles) => (
   <>
-    <Progress percent={Math.round(100/FormTitles.length - 1*(progress.progress))} />
+    <Progress percent={Math.round(100/(FormTitles.length - 1)*(progress.progress))} />
   </>
 );
 
