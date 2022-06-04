159b0ae7dff2c3eba3abd755893ee803afcda52b
moumou <hielf1984@gmail.com>
2022-06-03 17:28:11 -0700

    fix issue

--- app/javascript/src/Components/ProgressBar.js
+++ app/javascript/src/Components/ProgressBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Progress } from 'antd';
 
-const ProgressBar = (progress, FormTitles) => (
+const ProgressBar = ({progress, FormTitles}) => (
   <>
-    <Progress percent={Math.round(100/(FormTitles.length - 1)*(progress.progress))} />
+    <Progress percent={Math.round(100/(FormTitles.length-1)*(progress))} />
   </>
 );
 
