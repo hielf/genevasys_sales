22bd9e981e17f8fe9956c4fcd75f4ab747cbe1e6
moumou <hielf1984@gmail.com>
2022-08-25 13:10:44 -0700

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :default_env, {rvm_bin_path: '~/.rvm/bin'}
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 2
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
