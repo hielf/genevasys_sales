aa83b1d64ebc07455bd388f5d4617a93bc286368
moumou <hielf1984@gmail.com>
2022-09-19 12:36:31 -0700

    product ref modification

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,87 +5,35 @@ class Api::ProductsController < Api::ApplicationController
     products = Product.where(visible: 1, product_type: 1, status: 1)
 
     @bundles = Array.new
-    products.where("label LIKE ?", '%Bundle%').each do |product|
+    products.where("ref LIKE ?", 'A%').each do |product|
       hash = product.as_json
-      description = ApplicationController.helpers.string_between_markers(ActionView::Base.full_sanitizer.sanitize(hash["description"]), "(", ")").upcase
-      tag = Set.new
-      description.split("/").each do |d|
-        if d.scan(/\d/).join('').to_i > 0
-          p "B#{d.scan(/\d/).join('')}"
-          tag << "B#{d.scan(/\d/).join('')}"
-        end
-        if d.include?("TV")
-          tag << "C"
-        end
-        if d.include?("PHONE")
-          tag << "D"
-        end
-      end
+      tag = ApplicationController.helpers.product_tags(product)
       hash["tag"] = tag.sort
       @bundles << hash
     end
     @bundles = @bundles.sort_by { |k| k["level"] }.reverse!
 
     @internets = Array.new
-    products.where("label LIKE ?", '%Internet%').each do |product|
+    products.where("ref LIKE ?", 'B%').each do |product|
       hash = product.as_json
-      description = ActionView::Base.full_sanitizer.sanitize(hash["description"]).upcase
-      tag = Set.new
-      description.split("/").each do |d|
-        if d.scan(/\d/).join('').to_i > 0
-          p "B#{d.scan(/\d/).join('')}"
-          tag << "B#{d.scan(/\d/).join('')}"
-        end
-        if d.include?("TV")
-          tag << "C"
-        end
-        if d.include?("PHONE")
-          tag << "D"
-        end
-      end
+      tag = ApplicationController.helpers.product_tags(product)
       hash["tag"] = tag.sort
       @internets << hash
     end
     @internets = @internets.sort_by { |k| k["level"] }.reverse!
 
     @tv_box = Array.new
-    products.where("label LIKE ?", '%TV Box%').each do |product|
+    products.where("ref LIKE ?", 'C%').each do |product|
       hash = product.as_json
-      description = ActionView::Base.full_sanitizer.sanitize(hash["description"]).upcase
-      tag = Set.new
-      description.split("/").each do |d|
-        if d.scan(/\d/).join('').to_i > 0
-          p "B#{d.scan(/\d/).join('')}"
-          tag << "B#{d.scan(/\d/).join('')}"
-        end
-        if d.include?("TV")
-          tag << "C"
-        end
-        if d.include?("PHONE")
-          tag << "D"
-        end
-      end
+      tag = ApplicationController.helpers.product_tags(product)
       hash["tag"] = tag.sort
       @tv_box << hash
     end
 
     @ip_phone = Array.new
-    products.where("label LIKE ?", '%IP Phone%').each do |product|
+    products.where("ref LIKE ?", 'D%').each do |product|
       hash = product.as_json
-      description = ActionView::Base.full_sanitizer.sanitize(hash["description"]).upcase
-      tag = Set.new
-      description.split("/").each do |d|
-        if d.scan(/\d/).join('').to_i > 0
-          p "B#{d.scan(/\d/).join('')}"
-          tag << "B#{d.scan(/\d/).join('')}"
-        end
-        if d.include?("TV")
-          tag << "C"
-        end
-        if d.include?("PHONE")
-          tag << "D"
-        end
-      end
+      tag = ApplicationController.helpers.product_tags(product)
       hash["tag"] = tag.sort
       @ip_phone << hash
     end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -65,4 +65,67 @@ module ProductsHelper
     p count
   end
 
+  def product_tags(product)
+    # A101110
+    ref = product.ref
+    tag = Set.new
+    ref.chars.each_with_index do |char, index|
+      case ref[0]
+      when "A"
+        case index
+        when 1
+          sub_ref = "B" + char.rjust((ref.length - 1), "0")
+          p_b = Product.find_by(ref: sub_ref)
+          if p_b
+            description = ActionView::Base.full_sanitizer.sanitize(p_b.description).upcase
+            description.split("/").each do |d|
+              if d.scan(/\d/).join('').to_i > 0
+                p "B#{d.scan(/\d/).join('')}"
+                tag << "B#{d.scan(/\d/).join('')}"
+              end
+            end
+          end
+        when 2
+          sub_ref = "C" + char.rjust((ref.length - 1), "0")
+          p_c = Product.find_by(ref: sub_ref)
+          tag << "C" if p_c
+        when 3
+          sub_ref = "D" + char.rjust((ref.length - 1), "0")
+          p_d = Product.find_by(ref: sub_ref)
+          tag << "D" if p_d
+        when 4
+          sub_ref = "E" + char.rjust((ref.length - 1), "0")
+          p_e = Product.find_by(ref: sub_ref)
+          tag << "E" if p_e
+        when 5
+          sub_ref = "F" + char.rjust((ref.length - 1), "0")
+          p_f = Product.find_by(ref: sub_ref)
+          tag << "F" if p_f
+        end
+      when "B"
+        p_b = Product.find_by(ref: ref)
+        if p_b
+          description = ActionView::Base.full_sanitizer.sanitize(p_b.description).upcase
+          description.split("/").each do |d|
+            if d.scan(/\d/).join('').to_i > 0
+              p "B#{d.scan(/\d/).join('')}"
+              tag << "B#{d.scan(/\d/).join('')}"
+            end
+          end
+        end
+      when "C"
+        tag << "C"
+      when "D"
+        tag << "D"
+      when "E"
+        tag << "E"
+      when "F"
+        tag << "F"
+      end
+
+    end
+
+    return tag.sort
+  end
+
 end
