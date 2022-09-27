93290ef086e93fd1d44769bffd1ba916c53ff451
moumou <hielf1984@gmail.com>
2022-09-27 14:40:09 -0700

    fix document issue for antd mobile

--- app/javascript/src/MobileApp.js
+++ app/javascript/src/MobileApp.js
@@ -1,30 +1,14 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import OrderForm from './Mobile/Order/Form'
-import ResultForm from './Mobile/Order/ResultForm'
 import Term from './Mobile/Pages/Term'
-import { ConfigProvider } from 'antd';
-
-// ConfigProvider.config({
-//   theme: {
-//     primaryColor: '#90BA75',
-//     textColor: '#90BA75',
-//     textColorSecondary: '#90BA75',
-//     linkColor: '#90BA75',
-//   },
-// });
 
 const MobileApp = () => {
   return (
     <Switch>
       <Route exact path="/order/new" component={OrderForm}/>
-      <Route exact path="/order/submit" component={ResultForm}/>
-      <Route exact path="/terms" component={Term}/>
     </Switch>
   )
 }
 
 export default MobileApp
-
-// https://demos.creative-tim.com/material-kit-react/#/presentation
-// https://ant.design/index-cn
