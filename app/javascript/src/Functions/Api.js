534ac4367a82597fe9fa114187f0588e7f7d09bb
moumou <hielf1984@gmail.com>
2022-09-28 15:51:52 -0700

    Update Api.js

--- app/javascript/src/Functions/Api.js
+++ app/javascript/src/Functions/Api.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 
 let url;
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  url = `http://` + window.location.host + `/api/`
-} else {
-  url = `https://` + window.location.host + `/api/`
-}
+url = window.location.protocol + `//` + window.location.host + `/api/`
 
 export default axios.create({
   baseURL: url
