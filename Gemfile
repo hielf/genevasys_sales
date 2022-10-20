ea700c83f4cbe6003b71edbb9f633046ac98ec7c
moumou <hielf1984@gmail.com>
2022-10-19 17:09:35 -0700

    downgrade puma to 4.3.12

--- Gemfile
+++ Gemfile
@@ -15,8 +15,7 @@ gem 'mysql2'
 gem "rack", ">= 2.2.3.1"
 
 # Use Puma as the app server
-gem 'puma-daemon', require: false
-gem 'puma', '~> 5.6'
+gem 'puma', '~> 4.3.12'
 gem 'god'
 # Use SCSS for stylesheets
 # gem 'sassc-rails'
--- config/puma.rb
+++ config/puma.rb
@@ -4,9 +4,6 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-require 'puma/daemon'
-daemonize
-
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
@@ -17,7 +14,6 @@ port        ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
-
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
