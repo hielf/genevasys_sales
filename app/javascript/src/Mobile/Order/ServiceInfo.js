88ba3c1675886ce625dc0e5b81b1b31e1c7a166e
moumou <hielf1984@gmail.com>
2022-10-26 17:17:45 -0700

    datepicker for serviceinfo

--- app/javascript/src/Mobile/Order/ServiceInfo.js
+++ app/javascript/src/Mobile/Order/ServiceInfo.js
@@ -4,38 +4,45 @@ import { PickerView } from 'antd-mobile'
 
 const ServiceInfo = ({formData, setFormData}) => {
 
-  const onChange1 = (checkedValues) => {
+  const d1 = new Date();
+  const d2 = new Date();
+  const dates = [];
+  d2.setDate(d2.getDate() + 60);
 
+  while (d1 <= d2) {
+    d1.setDate(d1.getDate() + 1)
+    dates.push({ label: d1.toLocaleString('en', { month: 'short' }).toString() + ', ' + d1.getDate().toString(), value: (new Date(d1))});
   }
 
-  const dateColumns => {
-    let d1 = new Date();
-    let d2 = new Date(d1.setMonth(d1.getMonth() + 1));
-  }
-
-  const basicColumns = [
-    [
-      { label: 'Tue, 25', value: 'Tue, 25 Oct 2022' },
-      { label: 'Tue, 26', value: 'Tue, 26 Oct 2022' },
-      { label: 'Tue, 27', value: 'Tue, 27 Oct 2022' },
-      { label: 'Tue, 28', value: 'Tue, 28 Oct 2022' },
-      { label: 'Tue, 29', value: 'Tue, 29 Oct 2022' },
-      { label: 'Tue, 30', value: 'Tue, 30 Oct 2022' },
-      { label: 'Tue, 31', value: 'Tue, 31 Oct 2022' },
-      { label: 'Tue, 32', value: 'Tue, 32 Oct 2022' },
-      { label: 'Tue, 33', value: 'Tue, 33 Oct 2022' },
-      { label: 'Tue, 34', value: 'Tue, 34 Oct 2022' },
-    ],
-    [
-      { label: 'Morning', value: '1' },
-      { label: 'Afternoon', value: '2' },
-    ],
+  const periods = [
+    { label: 'Morning', value: 1 },
+    { label: 'Afternoon', value: 2 },
   ]
+  const [dateColumns, setDateColumns] = useState([dates, periods])
+
+  if (formData.dateRequest === "") {
+    setFormData((formData) => ({ ...formData, dateRequest: dates[2]["value"] }));
+    setFormData((formData) => ({ ...formData, installationTime: 1 }));
+  }
+
+  const onChange1 = (val, extend) => {
+    setFormData((formData) => ({ ...formData, dateRequest: val[0] }));
+    setFormData((formData) => ({ ...formData, installationTime: val[1] }));
+  }
 
   return (
       <Space direction='vertical' block>
         <p style={{ fontSize: "var(--adm-font-size-8)", textAlign: "center", fontWeight: 'bold', }}>Choose Date</p>
-        <PickerView columns={basicColumns} />
+        <PickerView
+          columns={dateColumns}
+          value={
+            [formData.dateRequest, formData.installationTime]
+          }
+          onChange={(val, extend) => {
+            onChange1(val, extend)
+            console.log('onChange', val, extend.items)
+          }}
+        />
         <p style={{ fontSize: "var(--adm-font-size-4)", }}>* A representative will call you later to confirm the installation time.</p>
       </Space>
   );
