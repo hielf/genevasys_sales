879296ffee78157ec797d6d791dd20d2f8a9f1f8
moumou <hielf1984@gmail.com>
2022-11-07 17:21:58 -0800

    edit customer info city list

--- app/javascript/src/Mobile/Order/CustomerInfo.js
+++ app/javascript/src/Mobile/Order/CustomerInfo.js
@@ -8,15 +8,20 @@ const CustomerInfo = ({formData, setFormData}) => {
 
   const basicColumns = [
     [
-      { label: '周一', value: 'Mon' },
-      { label: '周二', value: 'Tues' },
-      { label: '周三', value: 'Wed' },
-      { label: '周四', value: 'Thur' },
-      { label: '周五', value: 'Fri' },
-    ],
-    [
-      { label: '上午', value: 'am' },
-      { label: '下午', value: 'pm' },
+      { label: 'VANCOUVER', value: 'VANCOUVER' },
+      { label: 'BURNABY', value: 'BURNABY' },
+      { label: 'RICHMOND', value: 'RICHMOND' },
+      { label: 'WEST VANCOUVER', value: 'WEST VANCOUVER' },
+      { label: 'NORTH VANCOUVER', value: 'NORTH VANCOUVER' },
+      { label: 'NEW WESTMINSTER', value: 'NEW WESTMINSTER' },
+      { label: 'SURREY', value: 'SURREY' },
+      { label: 'WHITE ROCK', value: 'WHITE ROCK' },
+      { label: 'COQUITLAM', value: 'COQUITLAM' },
+      { label: 'PORT COQUITLAM', value: 'PORT COQUITLAM' },
+      { label: 'DELTA', value: 'DELTA' },
+      { label: 'LANGLEY', value: 'LANGLEY' },
+      { label: 'PORT MOODY', value: 'PORT MOODY' },
+      { label: 'MAPLE RIDGE', value: 'MAPLE RIDGE' },
     ],
   ]
 
