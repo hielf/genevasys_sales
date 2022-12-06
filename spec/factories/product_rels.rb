3561323fc367a3156c4379277c127d830805b792
moumou <hielf1984@gmail.com>
2022-12-06 13:01:09 -0800

    Merge with Mobile Branch 2

---
+++ spec/factories/product_rels.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product_rel do
+    upper_product_id { "MyString" }
+    lower_product_id { "MyString" }
+    start_date { "2022-10-17" }
+    end_date { "2022-10-17" }
+  end
+end
