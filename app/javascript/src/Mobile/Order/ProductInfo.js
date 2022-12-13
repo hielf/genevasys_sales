a8a21cb568446c18d23ca2900c50393c69b25435
moumou <hielf1984@gmail.com>
2022-12-13 10:51:08 -0800

    product info styling

--- app/javascript/src/Mobile/Order/ProductInfo.js
+++ app/javascript/src/Mobile/Order/ProductInfo.js
@@ -16,6 +16,20 @@ const ProductInfo = ({formData, setFormData}) => {
     setList(res);
   };
 
+  const descriptionSanitize = (description) => {
+    let descriptions = description.split("\r\n")
+
+    return (
+      <Space direction='vertical' style={ {'--gap': '6px', fontSize: "14px", color: "#8f8f8f", } }>
+        { descriptions.map(d => {
+          return (
+            <p key={ d } style={{ margin: '0px', fontFamily: "'Varela Round', sans-serif", }}>* { d }</p>
+          )
+        }) }
+      </Space>
+    )
+  };
+
   const onChange1 = (values) => {
     let listData = [];
     let tags = [];
@@ -74,22 +88,23 @@ const ProductInfo = ({formData, setFormData}) => {
                       list[p].map(d => {
                         return (
                           <Badge key={ d.value } color='#ff6800' content={ d.bundle_tag } style={{ '--right': '97%', }} bordered>
-                            <Radio value={d.value} style={{ borderStyle: "solid", borderWidth: "0.5px", borderColor: "#ccc", borderRadius: "10px", padding: "8px" }} block>
-                              <Grid columns={4} gap={8}>
-                                <Grid.Item span={3}>
+                            <Radio value={d.value} block style={{ borderStyle: "solid", borderWidth: "0.5px", borderColor: "#ccc", borderRadius: "10px", padding: "8px" }}>
+                              <Grid columns={8} gap={8}>
+                                <Grid.Item span={8}>
+                                  <p style={{ width: "100vh", marginTop: "8px", marginBottom: "0px", fontFamily: "'Oswald', sans-serif", }}>{d.label}</p>
+                                </Grid.Item>
+                                <Grid.Item span={5}>
                                   <Grid columns={5} gap={3}>
                                     <Grid.Item span={5}>
-                                      <span>{d.label}</span>
-                                    </Grid.Item>
-                                    <Grid.Item span={5}>
-                                      <span style={{ fontSize: "14px", color: "#8f8f8f" }}>{ d.description }</span>
+                                      { descriptionSanitize(d.description) }
                                     </Grid.Item>
                                   </Grid>
                                 </Grid.Item>
-                                <Grid.Item span={1} style={{ alignSelf: "center" }}>
-                                  <span style={{ fontSize: "24px", color: "#ff6800" }}>{ d.promotion_price }</span>
+                                <Grid.Item span={3} style={{ alignSelf: "center", textAlign: "right", }}>
+                                  <span style={{ fontSize: "14px", fontFamily: "'Varela Round', sans-serif", color: "#ff6800" }}>$</span>
+                                  <span style={{ fontSize: "24px", fontFamily: "'Oswald', sans-serif", color: "#ff6800" }}>{ d.promotion_price }</span>
                                   <br/>
-                                  <span style={{ fontSize: "14px", textDecoration:"line-through", color: "#8f8f8f", textDecorationColor: "#a1a1a1", textDecorationThickness: "0.5px",}}>{ d.price }</span>
+                                  <span style={{ fontSize: "14px", fontFamily: "'Oswald', sans-serif", textDecoration:"line-through", color: "#8f8f8f", textDecorationColor: "#a1a1a1", textDecorationThickness: "0.5px",}}>{ d.price }</span>
                                 </Grid.Item>
                               </Grid>
                             </Radio>
