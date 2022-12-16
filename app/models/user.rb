078423b89372bb26b03bda37c95bdde3678a920b
moumou <hielf1984@gmail.com>
2022-12-15 17:26:44 -0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,10 @@ class User < ApplicationRecord
     return log.op_type, log.op_message
   end
 
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
+
   # def had_subscribtion?(package)
   #   subscribtions.find_by(package_type: package.package_type)
   # end
